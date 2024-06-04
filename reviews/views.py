67b96b2630cc002a92022c94d87046a5f96ea0e3
Sasha Moore <sansopanso@gmail.com>
2024-06-04 11:43:14 +0300

    Chapter_1

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# def index(request):
+#     name = request.GET.get("name") or "world"
+#     return HttpResponse(f"Hello {name}!")
+
+
+def index(request):
+    name = "World"
+    return render(request, "base.html", {"name": name})
+    # return render(request, "base.html")
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
